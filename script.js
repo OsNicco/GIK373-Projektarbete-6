1f3a3b36412a1fdb66227fab730aefe374f844e8
OsNicco <Niccopallard@gmial.com>
2026-05-09 21:21:19 +0200

    Lite ide tester

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+const bottomBar = document.querySelector('.bottom-bar');
+const nav = document.querySelector('nav');
+
+window.addEventListener('scroll', () => {
+  const navBottom = nav.getBoundingClientRect().bottom;
+  
+if (navBottom < 0) {
+  bottomBar.style.transform = 'translateX(-50%) translateY(0)';
+} else {
+  bottomBar.style.transform = 'translateX(-50%) translateY(100px)';
+}
+});
