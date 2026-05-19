a24fd7fbe57edb2e849d1f9eacd59ddd60a10d22
OsNicco <niccopallard@gmail.com>
2026-05-19 11:00:04 +0200

    .

--- visa_data_test/script.js
+++ visa_data_test/script.js
@@ -1,55 +1,13 @@
 const urlSCB =
-  "https://api.scb.se/OV0104/v1/doris/sv/ssd/START/BE/BE0101/BE0101A/BefolkningNy";
+  "https://api.scb.se/OV0104/v1/doris/sv/ssd/START/MI/MI0307/MI0307T2N";
 
 const querySCB = {
   query: [
     {
-      code: "Region",
-      selection: {
-        filter: "vs:RegionKommun07",
-        values: [
-          "0120",
-          "2021",
-          "2023",
-          "2026",
-          "2029",
-          "2031",
-          "2034",
-          "2039",
-          "2061",
-          "2062",
-          "2080",
-          "2081",
-          "2082",
-          "2083",
-          "2084",
-          "2085",
-        ],
-      },
-    },
-    {
       code: "ContentsCode",
       selection: {
         filter: "item",
-        values: ["BE0101N1"],
-      },
-    },
-    {
-      code: "Tid",
-      selection: {
-        filter: "item",
-        values: [
-          "2015",
-          "2016",
-          "2017",
-          "2018",
-          "2019",
-          "2020",
-          "2021",
-          "2022",
-          "2023",
-          "2024",
-        ],
+        values: ["0000047A", "00000479", "00000478"],
       },
     },
   ],
@@ -60,6 +18,9 @@ const querySCB = {
 
 const request = new Request(urlSCB, {
   method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
   body: JSON.stringify(querySCB),
 });
 
@@ -68,62 +29,36 @@ fetch(request)
   .then((data) => printSCBChart(data));
 
 function printSCBChart(dataSCB) {
-  // lägg in all data i ett diagram
   console.log(dataSCB);
 
-  const years = dataSCB.data.splice(0, 10);
-  console.log(years);
+  // labels från key
+  const labels = dataSCB.data.map((item) => item.key[0]);
 
-  const labels = years.map((year) => year.key[1]);
   console.log(labels);
 
-  const data = years.map((year) => year.values[0]);
+  // värden
+  const data = dataSCB.data.map((item) => Number(item.values[0]));
+
   console.log(data);
 
   const dataset = [
     {
-      label: "Befolkning per år i Vansbro",
+      label: "Miljöstatistik",
       data,
       borderWidth: 2,
-      borderColor: "hsla(250, 100%, 30%, 1)",
-      hoverBorderWidth: 4,
+      backgroundColor: [
+        "rgba(54, 162, 235, 0.6)",
+        "rgba(255, 99, 132, 0.6)",
+        "rgba(75, 192, 192, 0.6)",
+      ],
     },
   ];
 
   new Chart(document.getElementById("scb"), {
     type: "bar",
-    data: { labels, datasets: dataset },
-  });
-}
-
-const urlUN =
-  "https://unstats.un.org/SDGAPI/v1/sdg/DataAvailability/GetIndicatorsAllCountries";
-
-const requestUN = new Request(urlUN, {
-  method: "POST",
-  body: "dataPointType=1&countryId=0&natureOfData=all",
-  headers: { "Content-Type": "application/x-www-form-urlencoded" },
-});
-
-fetch(requestUN)
-  .then((response) => response.json())
-  .then((data) => printUNChart(data));
-
-function printUNChart(dataUN) {
-  console.log(dataUN);
-  const indicators = dataUN[6].indicators;
-  console.log(indicators);
-
-  const labels = indicators.map((indicator) => indicator.code);
-  console.log(labels);
-
-  const data = indicators.map((indicator) => indicator.percentage);
-  console.log(data);
-
-  const datasets = [{ label: "Uppfyllnad per indikator för mål 7 (%)", data }];
-
-  new Chart(document.getElementById("un"), {
-    type: "bar",
-    data: { labels, datasets },
+    data: {
+      labels,
+      datasets: dataset,
+    },
   });
 }
