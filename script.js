775a04de0adb78579025a3c19c641727ec786d8d
OsNicco <niccopallard@gmail.com>
2026-05-19 10:11:08 +0200

    .

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
+const bottomBar = document.querySelector(".bottom-bar");
+const nav = document.querySelector("nav");
 
-
-const bottomBar = document.querySelector('.bottom-bar');
-const nav = document.querySelector('nav');
-
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", () => {
   const navBottom = nav.getBoundingClientRect().bottom;
-  
-if (navBottom < 0) {
-  bottomBar.style.transform = 'translateX(-50%) translateY(0)';
-} else {
-  bottomBar.style.transform = 'translateX(-50%) translateY(100px)';
-}
-});
 
+  if (navBottom < 0) {
+    bottomBar.style.transform = "translateX(-50%) translateY(0)";
+  } else {
+    bottomBar.style.transform = "translateX(-50%) translateY(100px)";
+  }
+});
 
 // ============================================================================================================================= //
 
+const RecyUrl =
+  "https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/dataflow/ESTAT/env_waspacr$defaultview/1.0?references=descendants&detail=referencepartial";
+
+const test = await fetch(RecyUrl).then((response) => response.json());
+console.log(test);
